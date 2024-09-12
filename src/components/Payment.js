ebb94d740924e361dc49e488b0a1ac28e63a0b4d
ff1502 <74197166+ff1502@users.noreply.github.com>
2024-09-12 05:49:47 +0200

    1.0.5 hotfix Guthaben Info

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { updateBalance } from '../utils/balanceUtils'; // Guthaben-Update Funktion
 import { auth } from '../firebase';
 import { logUserAction } from '../helpers/logging'; // Log-Funktion
 
@@ -27,25 +26,21 @@ function Payment() {
     }
 
     if (paymentMethod === 'paypal') {
-      // Weiterleitung zu PayPal.me für die Zahlung
       alert(`Sie werden zu PayPal weitergeleitet, um ${amount}€ zu bezahlen.`);
       window.location.href = `https://www.paypal.me/PrutheniaBierkasse/${amount}`;
       setPaymentConfirmed(true);
 
-      // Keine Guthaben-Aktualisierung in Firebase mehr
       await logUserAction(auth.currentUser.uid, 'Guthaben aufgeladen', `Betrag: ${amount}€ (PayPal)`);
       
     } else if (paymentMethod === 'banktransfer') {
       alert(`Bitte überweise ${amount}€ gemäß den Banküberweisungsanweisungen.`);
       setPaymentConfirmed(true);
 
-      // Nur Logging ohne Firebase-Guthaben-Aktualisierung
       await logUserAction(auth.currentUser.uid, 'Guthaben per Banküberweisung angefragt', `Betrag: ${amount}€`);
     } else {
       alert('Bitte wähle eine Zahlungsart.');
     }
 
-    // Weiterleitung nach Bestätigung
     navigate('/drinklist');
   };
 
@@ -77,6 +72,31 @@ function Payment() {
             <option value="banktransfer">Banküberweisung</option>
           </select>
         </div>
+
+        {paymentMethod === 'banktransfer' && (
+          <div className="alert alert-info mt-3">
+            <h4>Wichtige Hinweise zur Banküberweisung</h4>
+            <p>
+              Dein Guthaben wird aktualisiert, sobald das Geld auf dem Bankkonto eingegangen ist. <br></br>
+              Bitte überweise den Betrag auf das angegebene Konto. Die Bearbeitung kann einige Tage dauern. <br></br>
+
+              Kontoinhaber: Florian Förster <br></br>
+              IBAN: DE74 3101 0833 9912 9527 13 <br></br>
+              BIC: SCFBDE33XXX        <br></br>
+
+            </p>
+          </div>
+        )}
+
+        {paymentMethod === 'paypal' && (
+          <div className="alert alert-info mt-3">
+            <h4>Hinweis zu PayPal</h4>
+            <p>
+              Dein Guthaben wird aktualisiert, sobald die Zahlung auf dem PayPal-Konto eingegangen ist. <br></br>
+              Nach Abschluss der Zahlung wirst du automatisch zur Getränkeliste weitergeleitet.<br></br>
+            </p>
+          </div>
+        )}
 
         {paymentConfirmed && (
           <div className="alert alert-success mt-3">
